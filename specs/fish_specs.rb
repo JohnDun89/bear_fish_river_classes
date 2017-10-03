54d52354aa58e5da02c9a26556cc4faa59599c84
John Duncan <john@johnduncanphoto.com>
2017-10-03 17:23:06 +0100

    fish specs defined and test created

--- specs/fish_specs.rb
+++ specs/fish_specs.rb
@@ -3,3 +3,15 @@ require('minitest/autorun')
 require('minitest/rg')
 
 class TestFish < MiniTest::Test
+
+
+
+  def setup
+      @fish = Fish.new('Tom')
+  end
+
+  def test_name
+      assert_equal('Tom', @fish.name)
+  end
+
+end
